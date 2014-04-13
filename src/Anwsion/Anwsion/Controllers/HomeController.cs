57dbf395e15963e883bb28c3fa71874977dda4a1
yuexiaoyun <sfreee@gmail.com>
2014-04-13 20:50:51 +0800

    新建项目

---
+++ src/Anwsion/Anwsion/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Anwsion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getitem(int page = 1, int pagesize = 25)
+        {
+            var time = DateTime.Now.ToString("yyy-mm-dd HH:MM:ss");
+
+            var offset = (page - 1) * pagesize;
+            var pagetotal = page * pagesize;
+            var list = new List<dynamic>();
+            for (int i = 1 + offset; i <= pagetotal && i <= 50; i++)
+            {
+                list.Add(new { id = i, name = string.Format("item{0}", i), time = time });
+            }
+
+
+            var result = new
+            {
+                list = list
+            };
+            System.Threading.Thread.Sleep(1000);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ src/Anwsion/Anwsion/Scripts/list.js
@@ -0,0 +1,200 @@
+﻿var OFFSET = 5;
+var page = 1;
+var PAGESIZE = 20;
+
+var myScroll,
+	pullDownEl, pullDownOffset,
+	pullUpEl, pullUpOffset,
+	generatedCount = 0;
+var maxScrollY = 0;
+
+var hasMoreData = false;
+
+document.addEventListener('touchmove', function(e) {
+	e.preventDefault();
+}, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+	$(document).ready(function() {
+		loaded();
+	});
+}, false);
+
+function loaded() {
+	pullDownEl = document.getElementById('pullDown');
+	pullDownOffset = pullDownEl.offsetHeight;
+	pullUpEl = document.getElementById('pullUp');
+	pullUpOffset = pullUpEl.offsetHeight;
+
+	hasMoreData = false;
+	// $("#thelist").hide();
+	$("#pullUp").hide();
+
+	pullDownEl.className = 'loading';
+	pullDownEl.querySelector('.pullDownLabel').innerHTML = '正在努力加载...';
+
+	page = 1;
+	$.post(
+		"/home/getitem", {
+			"page": page,
+			"pagesize": PAGESIZE
+		},
+		function(response, status) {
+			if (status == "success") {
+				$("#thelist").show();
+
+				if (response.list.length < PAGESIZE) {
+					hasMoreData = false;
+					$("#pullUp").hide();
+				} else {
+					hasMoreData = true;
+					$("#pullUp").show();
+				}
+
+				// document.getElementById('wrapper').style.left = '0';
+
+				myScroll = new iScroll('wrapper', {
+					useTransition: true,
+					topOffset: pullDownOffset,
+					onRefresh: function() {
+						if (pullDownEl.className.match('loading')) {
+							pullDownEl.className = 'idle';
+							pullDownEl.querySelector('.pullDownLabel').innerHTML = '下拉更新提问';
+							this.minScrollY = -pullDownOffset;
+						}
+						if (pullUpEl.className.match('loading')) {
+							pullUpEl.className = 'idle';
+							pullUpEl.querySelector('.pullUpLabel').innerHTML = '上拉加载更多';
+						}
+					},
+					onScrollMove: function() {
+						if (this.y > OFFSET && !pullDownEl.className.match('flip')) {
+							pullDownEl.className = 'flip';
+							pullDownEl.querySelector('.pullDownLabel').innerHTML = '松开即可更新';
+							this.minScrollY = 0;
+						} else if (this.y < OFFSET && pullDownEl.className.match('flip')) {
+							pullDownEl.className = 'idle';
+							pullDownEl.querySelector('.pullDownLabel').innerHTML = '下拉更新提问';
+							this.minScrollY = -pullDownOffset;
+						} 
+						if (this.y < (maxScrollY - pullUpOffset - OFFSET) && !pullUpEl.className.match('flip')) {
+							if (hasMoreData) {
+								this.maxScrollY = this.maxScrollY - pullUpOffset;
+								pullUpEl.className = 'flip';
+								pullUpEl.querySelector('.pullUpLabel').innerHTML = '松开加载更多';
+							}
+						} else if (this.y > (maxScrollY - pullUpOffset - OFFSET) && pullUpEl.className.match('flip')) {
+							if (hasMoreData) {
+								this.maxScrollY = maxScrollY;
+								pullUpEl.className = 'idle';
+								pullUpEl.querySelector('.pullUpLabel').innerHTML = '上拉加载更多';
+							}
+						}
+					},
+					onScrollEnd: function() {
+						if (pullDownEl.className.match('flip')) {
+							pullDownEl.className = 'loading';
+							pullDownEl.querySelector('.pullDownLabel').innerHTML = '正在努力加载...';
+							// pullDownAction(); // Execute custom function (ajax call?)
+							refresh();
+						}
+						if (hasMoreData && pullUpEl.className.match('flip')) {
+							pullUpEl.className = 'loading';
+							pullUpEl.querySelector('.pullUpLabel').innerHTML = '正在努力加载...';
+							// pullUpAction(); // Execute custom function (ajax call?)
+							nextPage();
+						}
+					}
+				});
+
+				//$("#thelist").empty();
+				$.each(response.list, function(key, value) {
+					$("#thelist").append('<li>' + value.name + '\t' + value.time + '</li>');
+				});
+				// $("#thelist").listview("refresh");
+				myScroll.refresh(); // Remember to refresh when contents are loaded (ie: on ajax completion)
+				// pullDownEl.className = 'idle';
+				// pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Pull down to refresh...';
+				// this.minScrollY = -pullDownOffset;
+
+				if (hasMoreData) {
+					myScroll.maxScrollY = myScroll.maxScrollY + pullUpOffset;
+				} else {
+					myScroll.maxScrollY = myScroll.maxScrollY;
+				}
+				maxScrollY = myScroll.maxScrollY;
+			};
+		},
+		"json");
+}
+
+function refresh() {
+	page = 1;
+	$.post(
+		"/home/getitem", {
+			"page": page,
+			"pagesize": PAGESIZE
+		},
+		function(response, status) {
+			if (status == "success") {
+				$("#thelist").empty();
+
+				myScroll.refresh();
+
+				if (response.list.length < PAGESIZE) {
+					hasMoreData = false;
+					$("#pullUp").hide();
+				} else {
+					hasMoreData = true;
+					$("#pullUp").show();
+				}
+
+				$.each(response.list, function(key, value) {
+					$("#thelist").append('<li>' + value.name + '\t' + value.time + '</li>');
+				});
+				// $("#thelist").listview("refresh");
+				myScroll.refresh(); // Remember to refresh when contents are loaded (ie: on ajax completion)
+
+				if (hasMoreData) {
+					myScroll.maxScrollY = myScroll.maxScrollY + pullUpOffset;
+				} else {
+					myScroll.maxScrollY = myScroll.maxScrollY;
+				}
+				maxScrollY = myScroll.maxScrollY;
+			};
+		},
+		"json");
+}
+
+function nextPage() {
+	page++;
+	$.post(
+		"/home/getitem", {
+			"page": page,
+			"pagesize": PAGESIZE
+		},
+		function(response, status) {
+			if (status == "success") {
+				if (response.list.length < PAGESIZE) {
+					hasMoreData = false;
+					$("#pullUp").hide();
+				} else {
+					hasMoreData = true;
+					$("#pullUp").show();
+				}
+
+				$.each(response.list, function(key, value) {
+					$("#thelist").append('<li>' + value.name + '\t' + value.time + '</li>');
+				});
+				// $("#thelist").listview("refresh");
+				myScroll.refresh(); // Remember to refresh when contents are loaded (ie: on ajax completion)
+				if (hasMoreData) {
+					myScroll.maxScrollY = myScroll.maxScrollY + pullUpOffset;
+				} else {
+					myScroll.maxScrollY = myScroll.maxScrollY;
+				}
+				maxScrollY = myScroll.maxScrollY;
+			};
+		},
+		"json");
+}
